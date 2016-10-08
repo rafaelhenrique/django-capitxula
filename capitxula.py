31ef0a7db9376b0b0431acadc205e94358ad0035
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2016-10-08 11:45:30 -0300

    Simulate behavior of MultipleChoiceField with widget CheckboxSelectMultiple

--- capitxula.py
+++ capitxula.py
@@ -1,17 +1,23 @@
 import os
 import random
-from django.forms import CharField
+from django.forms import CharField, CheckboxSelectMultiple, MultipleChoiceField
 
 BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 CARS_IMAGE_PATH = os.path.join(BASE_PATH, 'images', 'cars')
 
 
-class Captcha(CharField):
+class Captcha(MultipleChoiceField):
     image_filenames = os.listdir(CARS_IMAGE_PATH)
+    widget = CheckboxSelectMultiple
 
     def __init__(self, *args, **kwargs):
         super(Captcha, self).__init__(*args, **kwargs)
-
-        image = random.choice(self.image_filenames)
-        image_path = os.path.join(CARS_IMAGE_PATH, image)
-        self.label = image_path
+        self.choices = (
+            ("a", "A"),
+            ("b", "B"),
+        )
+        print(BASE_PATH)
+        print(CARS_IMAGE_PATH)
+        # image = random.choice(self.image_filenames)
+        # image_path = os.path.join(CARS_IMAGE_PATH, image)
+        # self.label = image_path
